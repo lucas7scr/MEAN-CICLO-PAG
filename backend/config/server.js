18760e9becb0274484b01d855f61ba47433a4ed9
lucas7scr <luucas_09@yahoo.com.br>
2017-04-25 04:18:55 -0300

    criado o server.js / conexao com o mongodb / loader.js para carregar config iniciais / config no package.json

---
+++ backend/config/server.js
@@ -0,0 +1,12 @@
+const port = 8080
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
+
+server.listen(port, function(){
+    console.log(`BACKEND ta rolando na porta ${port}.`)
+})
